0fe3156bf17e52e7f8f8b8be3f1784d194bddd59
Percy Molina <pmolinaso@unsa.edu.pe>
2025-09-21 01:57:41 -0500

    Commit 1 en feature/mejoras: agregando script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Script principal del sitio
+console.log("Sitio web cargado correctamente");
+
+function saludar() {
+    alert("¡Bienvenido a mi sitio web!");
+}
+
+// Ejecutar cuando cargue la página
+window.onload = function() {
+    console.log("Página lista");
+};
